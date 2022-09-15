908b210984ad26ff79643d4f9da9dcb17f9e0679
francofazzito <francofazzito7@gmail.com>
2022-09-15 20:43:38 -0300

    add pedidos cliente logueado

--- Aplicacion/Pedidos/GestorPedido.cs
+++ Aplicacion/Pedidos/GestorPedido.cs
@@ -1,6 +1,7 @@
 ﻿using Dominio;
 using Repositorio.Repositorios.Pedidos;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aplicacion.Pedidos
 {
@@ -9,5 +10,7 @@ namespace Aplicacion.Pedidos
         public void Subir(Computadora computadora, Cliente clienteLogueado) => new RepositorioPedidoAlta().Agregar(computadora, clienteLogueado);
 
         public IEnumerable<Pedido> Todos => new RepositorioPedidoSoloLectura().Todos;
+
+        public IEnumerable<Pedido> TodosClienteLogueado => new RepositorioPedidoSoloLectura().Todos.Where(e => e.IdCliente == SesionCliente.Logueado.Id);
     }
 }
